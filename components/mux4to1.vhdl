35492ebc90dc0c2c225d03107ed5109105d58166
João Vitor Chrisóstomo <Joo Vitor Chrisstomo>
2017-03-31 20:17:22 -0300

    Testing commits

---
+++ components/mux4to1.vhdl
@@ -0,0 +1,22 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+entity mux4to1 is port( 
+			Sel : in  std_logic_vector (1 downto 0);     
+			in0 : in  std_logic_vector (7 downto 0);     
+            in1 : in  std_logic_vector (7 downto 0);
+		    in2 : in  std_logic_vector (7 downto 0);
+		    in3 : in  std_logic_vector (7 downto 0);
+		    out0 : out std_logic_vector (7 downto 0)
+);		   
+end mux4to1;
+
+architecture behavioral of mux4to1 is
+begin
+	with Sel select
+		X <= in0 when "00",
+			in1 when "01",
+			in2 when "10",
+			in3 when "11",
+			'0'  when others;
+end behavioral;
---
+++ components/register8.vhdl
@@ -0,0 +1,26 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+entity register8 is port(
+	In0 : in std_logic_vector(7 downto 0);
+	Ld : in std_logic; 
+	Clk : in std_logic;
+	Clk : in std_logic;
+	Out0 : out std_logic_vector(7 downto 0)
+);
+end register8;
+
+architecture description of register8 is
+
+begin
+    process(Clk, Clr)
+    begin
+        if Clr = '1' then
+			Out0 <= "00000000";
+        elsif rising_edge(Clk) then
+            if Ld = '1' then
+				Out0 <= In0;
+            end if;
+        end if;
+    end process;
+end description;
