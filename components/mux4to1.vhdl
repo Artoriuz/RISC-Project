4bd74fddd5d41ace6ea70bffc8557374d9f7020b
João Vitor Rafael Chrisóstomo <joao00999@gmail.com>
2017-03-31 20:36:03 -0300

    Update mux4to1.vhdl

--- components/mux4to1.vhdl
+++ components/mux4to1.vhdl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
@@ -21,27 +20,3 @@ begin
 			in3 when "11",
 			'0'  when others;
 end behavioral;
-=======
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-
-entity mux4to1 is port( 
-			Sel : in  std_logic_vector (1 downto 0);     
-			in0 : in  std_logic_vector (7 downto 0);     
-            in1 : in  std_logic_vector (7 downto 0);
-		    in2 : in  std_logic_vector (7 downto 0);
-		    in3 : in  std_logic_vector (7 downto 0);
-		    out0 : out std_logic_vector (7 downto 0)
-);		   
-end mux4to1;
-
-architecture behavioral of mux4to1 is
-begin
-	with Sel select
-		X <= in0 when "00",
-			in1 when "01",
-			in2 when "10",
-			in3 when "11",
-			'0'  when others;
-end behavioral;
->>>>>>> 7ae28f3e1e324121b9ef80808201ef0e0864ed19
