8838c90e194d6d1c2d0bb15917f64ab99c36fda9
Artoriuz <joao00999@gmail.com>
2017-05-05 22:47:35 -0300

    typos

--- microprocessador/microprocessador.vhdl
+++ microprocessador/microprocessador.vhdl
@@ -22,8 +22,8 @@ component datapath is
 		saida : out std_logic_vector(7 downto 0);
 		mux0select : in std_logic_vector(2 downto 0); 
 		mux1select, mux2select : in std_logic_vector(1 downto 0);
-		regload : in std_logic_vector(6 downto 0); -- ordem = Reg00 Reg01 Reg10 Reg11 Regout Regout Regcarryflag
-		regclear : in std_logic_vector(6 downto 0); -- ordem = Reg00 Reg01 Reg10 Reg11 Regout Regout Regcarryflag
+		regload : in std_logic_vector(6 downto 0); -- ordem = Reg00 Reg01 Reg10 Reg11 Regin Regout Regcarryflag
+		regclear : in std_logic_vector(6 downto 0); -- ordem = Reg00 Reg01 Reg10 Reg11 Regin Regout Regcarryflag
 		alucontrol : in std_logic_vector(3 downto 0)
 	);
 end component;
